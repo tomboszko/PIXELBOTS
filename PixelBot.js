8769d709cb218588a764f639c913736ee508874c
tomboszko <gomreethomas@gmail.com>
2023-11-14 20:55:55 +0100

    added pixelbots metadata

---
+++ PixelBot.js
@@ -0,0 +1,41 @@
+
+// test json file for metadata of the NFTs
+  {
+    "name": "Pixel Robot #1",
+    "description": "This is a unique pixel art robot from the Pixel Robot collection.",
+    "image": "https://yourwebsite.com/path/to/robot1.png",
+    "attributes": [
+      {
+        "trait_type": "Series",
+        "value": "PIXELBOTS"
+      },
+      {
+        "trait_type": "Robot Number",
+        "value": 1
+      }
+      {
+        "trait_type": "Style",
+        "value": "RetroWave"
+      }
+    ]
+}
+
+{
+    "name": "Pixel Robot #1",
+    "description": "This is a unique pixel art robot from the Pixel Robot collection.",
+    "image": "https://yourwebsite.com/path/to/robot1.png",
+    "attributes": [
+      {
+        "trait_type": "Series",
+        "value": "PIXELBOTS"
+      },
+      {
+        "trait_type": "Robot Number",
+        "value": 2
+      }
+      {
+        "trait_type": "Style",
+        "value": "Kawaii"
+      }
+    ]
+}
