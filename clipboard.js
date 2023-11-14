01802c0f5cc2f78b6ebc7d55658ece82f4d79883
tomboszko <gomreethomas@gmail.com>
2023-11-14 19:50:20 +0100

    Update security + refactoring index

--- clipboard.js
+++ clipboard.js
@@ -1,12 +1,12 @@
-// Function to copy wallet ID to clipboard when clicked
+// Function to copy wallet ID to clipboard on click
 window.copyToClipboard = function() {
     let walletIdElement = document.getElementById('walletIdDisplay');
     let walletId = walletIdElement.title;
     console.log('walletId:', walletId);
     console.log('navigator.clipboard:', navigator.clipboard);
     navigator.clipboard.writeText(walletId).then(function() {
-        console.log('Copying to clipboard was successful!');
         walletIdElement.title = 'Copied!';
+        console.log('Copying to clipboard was successful!');
         setTimeout(function() {
             walletIdElement.title = walletId;
         }, 2000); // Reset the tooltip after 2 seconds
