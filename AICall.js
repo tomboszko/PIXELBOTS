775f416a78997ae4f188a85bc683e058ddfaf792
tomboszko <gomreethomas@gmail.com>
2023-11-15 20:08:39 +0100

    Added openAi node

--- AICall.js
+++ AICall.js
@@ -1,6 +1,22 @@
-
 require('dotenv').config();
 
 const apiKey = process.env.API_KEY;
 
-console.log(process.env.API_KEY);
+console.log(apiKey);
+
+const OpenAI = require('openai');
+
+const openai = new OpenAI(apiKey);
+
+//test call to openai
+
+async function main() {
+    const chatCompletion = await openai.chat.completions.create({
+        messages: [{ role: 'user', content: 'Say this is a test' }],
+        model: 'gpt-3.5-turbo',
+    });
+
+    console.log(chatCompletion);
+}
+
+main();
